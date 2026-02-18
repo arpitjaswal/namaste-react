ac2e065512c6e19bf1029131cd1ed8163a7b0468
arpitjaswal <iamarpitjaswal@gmail.com>
2026-02-18 06:42:12 -0500

    react is just js, cdn links exploration, creating element/div using pure react/reactdom imports

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+const anchor = React.createElement("a",{id:"anc",href:"https://www.dhli.in/uploaded_files/resumes/resume_3404.pdf"},"react")
+const likhde = React.createElement("h3",{id:"likh"},"terimkc")
+const parent  = React.createElement("div",{id:"pyo"},[anchor,likhde])
+const rootDiv = ReactDOM.createRoot(document.getElementById("root"))
+const pyodapyo = React.createElement("div",{id:'saareyaanDaBudhdha'},[parent,parent])
+rootDiv.render(pyodapyo) 
+
+
+
+
+
+
