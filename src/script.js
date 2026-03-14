94ab0090d66cd9285e6d8ce3b5b52cff54a0f3b5
Arpit Jaswal <iamarpitjaswal@gmail.com>
2026-03-14 15:13:54 +0530

    lazy loading/dynamic bundling/chunking

--- src/script.js
+++ src/script.js
@@ -2,21 +2,22 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./script.css"
 import Header from "./components/Header";
-import Body from "./components/Body";
+
 import { Search } from "./components/Search";
-import { useState } from "react";
+import { useState,lazy,Suspense } from "react";
 import { createBrowserRouter,RouterProvider, Outlet } from "react-router";
-import AboutUs  from "./components/AboutUs";
+
 import AboutUsCombined  from "./components/AboutUsCombined";
 import { ContactUs } from "./components/ContactUs";
 import { Error } from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu"
 import { useCheckOnlineStatus } from "./customHooks/useCheckOnlineStatus.js";
 import cat from "./assets/touch_grass_offline.jpeg"
+
+const AboutUs = lazy(()=>import("./components/AboutUs"))
+const Body = lazy(()=>import("./components/Body"))
 const AppLayout = ()=>{
     const onlineStatus = useCheckOnlineStatus();
-    
-    
     if(onlineStatus==false){
         return<div style={{display:"flex",alignItems:"center",justifyContent:"center",
         height:"100vh",width:"100vw"
@@ -25,14 +26,10 @@ const AppLayout = ()=>{
         <h1>You are offline. Check your internet!</h1>
     </div>
     }
-
     return <div>
     <Header/>
     <div id="btn-container">
-        
-               
             </div>
-     
         <Outlet/>
     </div>
 }
@@ -43,11 +40,15 @@ const appRouter = createBrowserRouter([
         element:<AppLayout />,
         children:[{
             path:"/",
-            element:<Body />
+            element:<Suspense fallback={<div>Loading body..</div>}>
+           <Body />
+        </Suspense>
         },
     {
         path:"/aboutus", 
-        element:<AboutUs />
+        element:<Suspense fallback={<div>Loading about us...</div>}>
+            <AboutUs />
+        </Suspense>
     },
     {
         path:"/contactus",
