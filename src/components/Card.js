79cc402ca78d2aca801c3ca573dbeab6b27bf62d
Arpit Jaswal <iamarpitjaswal@gmail.com>
2026-03-21 17:55:33 -0400

    tame impala: old ways playing...

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ export const Card =({ele,index,setcurrentindex,ci})=>{
                     { ci==index && ele?.menu?.categories?.map((it)=><div className="bg-red-200 m-2 p-10 flex-col" key={it?.category?.id} >
                         {it?.category?.name || ""} 
                         {it?.category?.items?.map((i)=><div className=" bg-red-400 flex justify-evenly items-center" key={i?.item?.id}>
-                            <h6 className="bg-amber-300 border-2 m-4 p-4 ">
+                            <h6 className="bg-amber-300 border-2 m-4 p-4 w-200">
                             {i?.item?.name}
                             </h6>
                             <img className="w-30 h-30" src={i?.item?.item_image_url || "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}></img>
--- src/components/Item.js
+++ src/components/Item.js
@@ -9,7 +9,7 @@ export const Item = ({it})=>{
         showItem && <div className="bg-red-400 m-2 p-2 " key={it?.category?.id} >
                         {it?.category?.name || ""} 
                         {it?.category?.items?.map((i)=><div className="flex border-4" key={i?.item?.id}>
-                            <h6 className="bg-amber-300 border-2" >
+                            <h6 className="bg-amber-300 border-2 " >
                             {i?.item?.name}
                             </h6>
                             <img className="w-50 h-50" src={i?.item?.item_image_url || "http://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}></img>
